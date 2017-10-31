d79d3b436ee5a735742fc2dd11994b0349e04526
Oladeji Famakinwa <ojf27@cam.ac.uk>
2017-10-31 11:26:19 +0000

    fixed display of notices bug

--- js/primo-explore-cam-display-notices.js
+++ js/primo-explore-cam-display-notices.js
@@ -1,46 +1,55 @@
 (function(){
 "use strict";
 
-var secondToRemoveAlert = 1000;
+var secondToRemoveAlert = 3000;
 
-app.controller('PrmSearchbarAfterController', ['$location', '$mdDialog', '$timeout', '$mdToast', function ($location, $mdDialog, $timeout, $mdToast) {
+app.controller('PrmSearchbarAfterController', ['$location', '$mdDialog', '$timeout', '$mdToast', '$translate', 
+        function ($location, $mdDialog, $timeout, $mdToast, $translate) {
     var vm = this;
 
     var notice = '<span translate="nui.idiscovernotice"></span>';
-    var templateString = '<div layout="row" layout-align="center center" style="width: 100%" class="bar alert-bar layout-align-center-center layout-row">' + '<span class="md-toast-text">' + notice + '</span>' + '<md-divider class="md-primoExplore-theme"></md-divider>' + '<button class="md-button md-primoExplore-theme md-ink-ripple" type="button" ng-click="ctrl.onDismiss()" aria-label="DISMISS">' + '<span class="ng-scope" translate="nui.message.dismiss">DISMISS</span>' + '</button>' + '</div>';
+    var templateString = '<div layout="row" layout-align="center center" style="width: 100%" class="bar alert-bar layout-align-center-center layout-row">' 
+            + '<span class="md-toast-text">' + notice + '</span>' 
+            + '<md-divider class="md-primoExplore-theme"></md-divider>' 
+            + '<button class="md-button md-primoExplore-theme md-ink-ripple" type="button" ng-click="ctrl.onDismiss()" aria-label="DISMISS">' 
+            + '<span class="ng-scope" translate="nui.message.dismiss">DISMISS</span>' + '</button>' + '</div>';
 
     console.log('notice');
-    console.log(notice);
-    console.log(angular.element($("translate[value='nui.idiscovernotice']")));
+    
+    $translate('nui.idiscovernotice').then((translation) => {
+        vm.translation = translation;
+        if (vm.translation !== 'idiscoversnotice' && vm.translation.trim() !== "") {
+            $mdToast.show({
+                controllerAs: 'ctrl',
+                controller: function controller() {
+                    this.onDismiss = function () {
+                        $mdToast.hide();
+                    };
+                    this.getPlaceHolders = function (cur) {
+                        var placeHolders = [[{
+                            prefix: '',
+                            code: cur,
+                            tag: { span: ['class=""'] }
+                        }]];
 
-    $mdToast.show({
-        controllerAs: 'ctrl',
-        controller: function controller() {
-            this.onDismiss = function () {
-                $mdToast.hide();
-            };
-            this.getPlaceHolders = function (cur) {
-                var placeHolders = [[{
-                    prefix: '',
-                    code: cur,
-                    tag: { span: ['class=""'] }
-                }]];
+                        return placeHolders;
+                    };
+                },
+                hideDelay: secondToRemoveAlert * 1000,
+                position: 'top right',
+                template: templateString
+            }).then(function () {
+                vm.onToastClose();
+            });
 
-                return placeHolders;
+            //Following code fixes a bug with mdToast who places "position: relative" on root element but doesn't remove it once it is closed.
+            //This causes md-dialog to not display correctly.
+            vm.onToastClose = function () {
+                angular.element(document.querySelector("primo-explore")).css('position', '');
             };
-        },
-        hideDelay: secondToRemoveAlert * 1000,
-        position: 'top right',
-        template: templateString
-    }).then(function () {
-        vm.onToastClose();
+
+        }
     });
-
-    //Following code fixes a bug with mdToast who places "position: relative" on root element but doesn't remove it once it is closed.
-    //This causes md-dialog to not display correctly.
-    vm.onToastClose = function () {
-        angular.element(document.querySelector("primo-explore")).css('position', '');
-    };
 
 }]);
 
